88cb83ad455df058beca39e7bb898e49582fd0eb
oakir <oakir@ecomundo.eu>
2022-06-05 11:12:08 +0200

    update favicon & title

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "WS Notes",
   viewport: "width=device-width,initial-scale=1",
 });
 
