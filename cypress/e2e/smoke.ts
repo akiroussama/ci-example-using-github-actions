bfd39d7730cc107994b646db04c13ab7e29e1b00
oakir <oakir@ecomundo.eu>
2022-06-05 10:52:20 +0200

    fix test KO

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -35,7 +35,7 @@ describe("smoke tests", () => {
     cy.findByRole("link", { name: /notes/i }).click();
     cy.findByText("No notes yet");
 
-    cy.findByRole("link", { name: /\+ new note/i }).click();
+    cy.findByRole("link", { name: /\+ 🌱 New Note/i }).click();
 
     cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
     cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
