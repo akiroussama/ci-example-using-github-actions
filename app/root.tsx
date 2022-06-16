d06a450ffe2dca3602a265f28499f49bc0b03d7c
oakir <oakir@ecomundo.eu>
2022-06-16 06:44:28 +0200

    read write

--- app/root.tsx
+++ app/root.tsx
@@ -17,9 +17,6 @@ import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
 // uncomment me to provoke eslint error for demo purposes
 // import { getUserFromSession } from "./session.server";
-const badFormat = {
-  name: 'badFormat',
-};
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
